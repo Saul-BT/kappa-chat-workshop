8caafa9b48a8d1419143119694af483aa956bf0d
Saul Blanco Tejero <saul.blanco.tejero@iesjulianmarias.es>
2020-04-04 20:18:28 +0200

    Problem [1-6] - Chat with yourself in real-time

---
+++ single-chat.js
@@ -0,0 +1,20 @@
+// Save this file as single-chat.js
+
+var hypercore = require('hypercore')
+var feed = hypercore('./single-chat-feed', {
+  valueEncoding: 'json'
+})
+
+process.stdin.on('data', function(data) {
+  feed.append({
+    type: 'chat-message',
+    nickname: 'cat-lover',
+    text: data.toString().trim(),
+    timestamp: new Date().toISOString()
+  })
+})
+
+feed.createReadStream({ live: true })
+  .on('data', function(data) {
+    console.log(`<${data.timestamp}> ${data.nickname}: ${data.text}`)
+  })
