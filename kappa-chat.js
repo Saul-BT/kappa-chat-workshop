6bf8337bce56853b49468ea4a2111f42834fa5ba
Saul Blanco Tejero <saul.blanco.tejero@iesjulianmarias.es>
2020-04-06 13:43:44 +0200

    Problem 11 - Read last 10 messages every time data changed

--- kappa-chat.js
+++ kappa-chat.js
@@ -14,9 +14,24 @@ var timestampView = list(memdb(), function(msg, next) {
 var core = kappa('./multichat', { valueEncoding: 'json' })
 core.use('chats', timestampView)
 
-core.writer('local', function(err, feed) {
-  feed.append({ type: 'chat-message', timestamp: new Date().toISOString() })
+core.ready(() => {
+  core.api.chats.tail(10, msgs => {
+    msgs.forEach((msg, i) => {
+      console.log(`${i} - ${msg.value.timestamp}: ${msg.value.text}`)
+    })
+  })
 })
 
-core.api.chats.read()
-  .on('data', console.log)
+core.writer('local', function(err, feed) {
+  process.stdin.on('data', function(data) {
+    feed.append({
+      type: 'chat-message',
+      nickname: 'tarod',
+      text: data.toString(),
+      timestamp: new Date().toISOString()
+    })
+  })
+})
+
+//core.api.chats.read()
+//  .on('data', console.log)
