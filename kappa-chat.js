1cd61e13ddb4ac792cc92f3be6d987649c708ad6
Saul Blanco Tejero <saul.blanco.tejero@iesjulianmarias.es>
2020-04-06 13:25:32 +0200

    Problem 10b - Reading with kappa-core

--- kappa-chat.js
+++ kappa-chat.js
@@ -1,7 +1,22 @@
 var kappa = require('kappa-core')
+var memdb = require('memdb')
+var list = require('kappa-view-list')
 
-var core = kappa('./multichat', {valueEncoding: 'json'})
-core.writer('local', function (err, feed) {
-  // `feed` is just a hypercore instance, just like the first few exercises.
-  console.log(feed.key.toString('hex'))
+var timestampView = list(memdb(), function(msg, next) {
+  if (msg.value.timestamp && typeof msg.value.timestamp === 'string') {
+    // sort on the 'timestamp' field
+    next(null, [msg.value.timestamp])
+  } else {
+    next()
+  }
 })
+
+var core = kappa('./multichat', { valueEncoding: 'json' })
+core.use('chats', timestampView)
+
+core.writer('local', function(err, feed) {
+  feed.append({ type: 'chat-message', timestamp: new Date().toISOString() })
+})
+
+core.api.chats.read()
+  .on('data', console.log)
